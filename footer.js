89b4b1f256723ed26e2009de7b3ca7f2350a7124
acarlseen <110647552+acarlseen@users.noreply.github.com>
2024-05-24 12:29:11 -0700

    site complete

---
+++ footer.js
@@ -0,0 +1,40 @@
+addEventListener('DOMContentLoaded', (e) => {
+document.getElementsByTagName('footer')[0].innerHTML= '\
+<div class="container">\
+    <div class="location">Find us: <br>\
+        &ensp;Big Strong Gym<br>\
+        &ensp;22 Acacia Ave.<br>\
+        &ensp;Alameda, CA 99999\
+    </div>\
+    <div class="socials">Follow us: <br>\
+        <ul>\
+            <li>\
+                <i class="fa-brands fa-x-twitter"></i>StrongTweets\
+            </li>\
+            <li>\
+                <i class="fa-brands fa-instagram"></i>Strongsta\
+            </li>\
+            <li>\
+                <i class="fa-brands fa-tiktok"></i>BigTok\
+            </li>\
+            <li>\
+                <i class="fa-brands fa-twitch"></i>BigDumbTire\
+            </li>\
+        </ul>\
+    </div>\
+    <div class="site">\
+        Site:\
+        <ul>\
+            <li>\
+                <a href="/index.html">Home</a>\
+            </li>\
+            <li>\
+                <a href="/About.html">About</a>\
+            </li>\
+            <li>\
+                <a href="/SignIn.html">Sign In</a>\
+            </li>\
+        </ul>\
+    </div>\
+</div>'
+})
---
+++ navbar.js
@@ -0,0 +1,16 @@
+document.write('<nav>\
+        <div class="brand"><a class="brand-link" href="/">Big Strong</a></div>\
+        <div>\
+            <ul class="nav-list">\
+                <li>\
+                    <a class="nav-item" href="/About.html">About Us</a>\
+                </li>\
+                <li>\
+                    <a class="nav-item" href="/SignUp.html">Sign Up</a>\
+                </li>\
+                <li>\
+                    <a class="nav-item" href="/SignIn.html">Sign In</a>\
+                </li>\
+            </ul>\
+        </div>\
+    </nav>');
